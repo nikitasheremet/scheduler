85fc60af26e47d29a5af866762ebe9156822d16f
nikitasheremet <nikita.sheremet253@gmail.com>
2019-10-02 09:24:49 -0400

    add tests for useVisualMode add transition and back functionality

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react"
+
+export default function useVisualMode(initial) {
+    const [mode, setMode] = useState(initial)
+    const [history, setHistory] = useState([initial]);
+    // console.log(history)
+    function transition(transition, replace = false) {
+        if (replace) {
+            if (history.length >= 2) history.pop()
+        }
+        setMode(transition)
+        setHistory([...history, transition])
+    }
+    function back() {
+        if (history.length > 1) {
+            setMode([...history].slice(-2, -1)[0])
+            setHistory([...history].slice(0, -1))
+        }
+    }
+
+    return { mode, transition, back }
+}
