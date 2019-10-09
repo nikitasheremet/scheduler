f9cf7dc2184016d8f3ebd4e80e97aba7f51fe8b1
nikitasheremet <nikita.sheremet253@gmail.com>
2019-10-09 13:51:44 -0400

    update api path inside useAppliacation Hook

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -38,9 +38,9 @@ export default function() {
 
   useEffect(() => {
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then(res => {
       const [days, appointments, interviewers] = res
       dispatch({
