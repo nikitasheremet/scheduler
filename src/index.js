6aac89749bf3ec9ae01f0d960bd898741aeabb59
nikitasheremet <nikita.sheremet253@gmail.com>
2019-10-09 14:58:05 -0400

    update routes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import "index.scss"
 import Application from "components/Application"
 
 import axios from "axios"
-
+axios.defaults.baseURL = "https://interview-schedulerlhl.herokuapp.com/"
+console.log("--->", process.env.REACT_APP_API_BASE_URL)
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
 }
